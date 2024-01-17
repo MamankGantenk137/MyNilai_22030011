710c6af81f7bfa0bdbdccc70358e62202bb28f8b
MamankGantenk137 <121016737+MamankGantenk137@users.noreply.github.com>
2024-01-17 17:43:06 +0700

    Add files via upload

---
+++ MyNilai_22030011_Febriansyah/MyNilai_22030011/src/mynilai_22030011/MyNilai_22030011.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mynilai_22030011;
+import View.View_MyNilai_22030011;
+
+/**
+ *
+ * @author ASUS TUF GAMING A15
+ */
+public class MyNilai_22030011 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            java.awt.EventQueue.invokeLater(() -> {
+        new View_MyNilai_22030011().setVisible(true);
+    });
+    }
+    
+}
